4c3e36d12267bcd17fdc5024553f9f11d9727b42
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-05-24 11:25:35 -0300

    atualização

--- index.js
+++ index.js
@@ -9,3 +9,33 @@ const gestao = document.querySelector('#btn-gestao');
 const sobre = document.querySelector('#btn-sobre');
 
 const principal = document.querySelector('#principal');
+
+home.addEventListener('click', (evt)=>{
+    selecionarAba(evt.target)
+    window.open('home.html', 'ifm-main')
+})
+novo.addEventListener('click', (evt)=>{
+    selecionarAba(evt.target)
+    window.open('novo.html', 'ifm-main')
+    
+})
+pesquisar.addEventListener('click', (evt)=>{
+    selecionarAba(evt.target)
+    window.open('pesquisar.html', 'ifm-main')
+})
+gestao.addEventListener('click', (evt)=>{
+    selecionarAba(evt.target)
+    window.open('gestao.html', 'ifm-main')
+})
+sobre.addEventListener('click', (evt)=>{
+    selecionarAba(evt.target)   
+    window.open('sobre.html', 'ifm-main')
+})
+const selecionarAba =  (el)=>{
+    const abas = [...document.querySelectorAll('.aba')]
+    console.log(abas)
+    abas.forEach((e)=>{
+        e.classList.remove('abaSelect')
+    })
+    el.classList.add('abaSelect')
+}
