35ca6f82b060093fffaa646d06daf1d1310e1381
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-05-24 10:26:25 -0300

    atualizção

---
+++ index.js
@@ -0,0 +1,11 @@
+const cabeçalho = document.querySelector('#cabecalho');
+
+const menu = document.querySelector('#menu');
+
+const home = document.querySelector('#btn-home');
+const novo = document.querySelector('#btn-novo');
+const pesquisar = document.querySelector('#btn-pesquisar');
+const gestao = document.querySelector('#btn-gestao');
+const sobre = document.querySelector('#btn-sobre');
+
+const principal = document.querySelector('#principal');
